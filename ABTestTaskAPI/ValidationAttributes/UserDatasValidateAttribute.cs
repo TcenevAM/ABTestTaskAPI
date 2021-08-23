f53b40c21772edff5092de8a5226e1f8b558802c
TcenevAM <tcenevalexander@gmail.com>
2021-08-23 20:49:07 +0400

    fix datas validate

--- ABTestTaskAPI/ValidationAttributes/UserDatasValidateAttribute.cs
+++ ABTestTaskAPI/ValidationAttributes/UserDatasValidateAttribute.cs
@@ -8,7 +8,7 @@ namespace ABTestTaskAPI.ValidationAttributes
         public override bool IsValid(object value)
         {
             User user = (User)value;
-            if (user.LastActivityDate > user.RegistrationDate)
+            if (user.LastActivityDate < user.RegistrationDate)
             {
                 ErrorMessage = $"У пользователя с Id {user.Id} дата последней активности меньше даты регистрации";
                 return false;
