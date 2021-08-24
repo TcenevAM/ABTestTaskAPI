0a2753af1d76abe303a282b623d0fc1893883f2f
TcenevAM <tcenevalexander@gmail.com>
2021-08-24 18:11:36 +0400

    fix typo

--- ABTestTaskAPI/ValidationAttributes/UserRegistationDateValidateAttribute.cs
+++ ABTestTaskAPI/ValidationAttributes/UserRegistationDateValidateAttribute.cs
@@ -1,7 +1,6 @@
 ﻿using ABTestTaskAPI.Models;
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.Globalization;
 
 namespace ABTestTaskAPI.ValidationAttributes
 {
@@ -13,7 +12,7 @@ namespace ABTestTaskAPI.ValidationAttributes
             var RegistrationDate = DateTime.Parse(value.ToString());
             if (RegistrationDate < DateTime.Now.AddYears(-20))
             {
-                ErrorMessage = $"Пользователь с {user.id} слишком старый";
+                ErrorMessage = $"Пользователь с {user.Id} слишком старый";
                 return false;
             }
             return true;
